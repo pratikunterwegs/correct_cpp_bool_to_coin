fe8215c0ac6c4129d9823a37323e300e480a20a1
Pratik Gupte <pratikgupte16@gmail.com>
2019-08-19 17:22:47 +0200

    rewrite program

--- main.cpp
+++ main.cpp
@@ -7,25 +7,34 @@
 /// function to test main
 int do_main(std::vector<std::string> cliArgs) noexcept
 {
-    // do whatever
-/*    if (cliArgs.size() != 2)
+    // check if vector size is not equal to 2
+    if (cliArgs.size() < 2)
     {
-        std::cerr << "too few or too many args";
-        exit(EXIT_FAILURE);
+        std::cerr << "too few args\n";
+        return 1;
     }
-    */
-    if (std::string(cliArgs[1]) == "true")
+    else if (cliArgs.size() > 2) {
+        std::cerr << "too many args\n";
+        return 1;
+    }
+
+    // check if vector is true or false and convert
+    if (cliArgs[1] == "true")
     {
         std::cout << "heads\n";
-        return 0;
-    } else if (std::string(cliArgs[1]) == "false")
-    {
-        std::cout << "tails\n";
-        return 0;
-    } else {
-        std::cerr << "nonsense args";
-        exit(EXIT_FAILURE);
+
     }
+    else if (cliArgs[1] == "false") {
+        std::cout << "tails\n";
+    }
+    // handle nonsense args
+    else {
+        std::cerr << "nonsense args\n";
+        return 1;
+    }
+
+    return 0;
+
 }
 
 /// main function
@@ -34,12 +43,15 @@ int main(int argc, char* argv[])
     // convert argv to string
     std::vector<std::string> cliArgs(argv, argv + argc);
 
-    // check that some args and no args cases are different
-    assert(do_main({"bool_to_coin","blabla"}) == 1);
-    assert(do_main({"bool_to_coin","true"}) == 0);
-    assert(do_main({"bool_to_coin","false"}) == 0);
-    assert(do_main({"bool_to_coin","true", "true"}) == 1);
-
+    // check no args case
+    assert(do_main({"bool_to_coin"}) == 1);
+    // check many args case
+    assert(do_main({"bool_to_coin", "some", "some"}) == 1);
+    // check true and false
+    assert(do_main({"bool_to_coin", "true"}) == 0);
+    assert(do_main({"bool_to_coin", "false"}) == 0);
+    // check nonsense
+    assert(do_main({"bool_to_coin", "nonsense"}) == 1);
 
     return do_main(cliArgs);
 }
