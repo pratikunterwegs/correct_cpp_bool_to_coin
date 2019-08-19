8fd17244e3fa68c3089494e40d39721b1286d57f
Pratik Gupte <pratikgupte16@gmail.com>
2019-08-19 16:53:14 +0200

    some changes

--- main.cpp
+++ main.cpp
@@ -16,15 +16,15 @@ int do_main(std::vector<std::string> cliArgs) noexcept
     if (std::string(cliArgs[1]) == "true")
     {
         std::cout << "heads\n";
+        return 0;
     } else if (std::string(cliArgs[1]) == "false")
     {
         std::cout << "tails\n";
+        return 0;
     } else {
         std::cerr << "nonsense args";
         exit(EXIT_FAILURE);
     }
-
-    return 0;
 }
 
 /// main function
@@ -34,11 +34,11 @@ int main(int argc, char* argv[])
     std::vector<std::string> cliArgs(argv, argv + argc);
 
     // check that some args and no args cases are different
-    assert(do_main({"bool_to_coin", "1"}) != do_main({"bool_to_coin", "2"}));
-    assert(do_main({"bool_to_coin","true", "true"}) == 1);
     assert(do_main({"bool_to_coin","blabla"}) == 1);
     assert(do_main({"bool_to_coin","true"}) == 0);
     assert(do_main({"bool_to_coin","false"}) == 0);
+    assert(do_main({"bool_to_coin","true", "true"}) == 1);
+
 
     return do_main(cliArgs);
 }
